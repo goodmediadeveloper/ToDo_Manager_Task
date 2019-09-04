ae16e24c02efcd37e69205cbdfc4e5ec979057e2
Dzmitry Prokharau <goodmediadeveloper@gmail.com>
2019-09-04 16:21:36 +0300

    Fixed bug exit(Error Route).Changed link_to on button_to

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
-  # belongs_to :article
+  # belongs_to :tasks
 
   # has_many :posts, dependent: :destroy
   # has_many :comments, dependent: :destroy
